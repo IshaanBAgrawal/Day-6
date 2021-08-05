6c1be54cc2436e62814a53cb533aefd6727f720d
Ishaan <agrawalishaan115@gmail.com>
2021-08-05 20:58:43 +0530

    Work of Day 6

---
+++ Functions.py
@@ -0,0 +1,13 @@
+# Built in Functions in Python:-
+# print("Hello!")
+
+# num_char = len("Hello!")
+# print(num_char)
+
+# Making our own Functions using python Keywords:-
+
+def my_function():
+  print("hello")
+  print("bye")
+
+my_function()
